26a4c33eab71fc4c3aa84f13071e9112160a452e
reflexson <131407662+reflexson@users.noreply.github.com>
2023-06-05 20:34:03 -0500

    initial

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+var startButton = document.querySelector("#startGame");
+startButton.addEventListener("click", wordGame);
+
+
+
+
+function wordGame() {
+  var timeLeft = 30;
+    var elem = document.getElementById('timer');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+      } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+      }
+    }
+
+}
